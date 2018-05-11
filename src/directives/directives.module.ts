3d1430e04fe81f9aa89c6dd80edea7d0878bafca
Nafis Bey <nbey@jarv.us>
2018-05-10 23:53:42 -0400

    Remove deprecated directive

--- src/directives/directives.module.ts
+++ src/directives/directives.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
-import { AccountTypeIconDirective } from './account-type-icon/account-type-icon';
+
 import { InAppBrowserLinkDirective } from './in-app-browser-link/in-app-browser-link';
 @NgModule({
-	declarations: [AccountTypeIconDirective,
-    InAppBrowserLinkDirective],
+	declarations: [
+		InAppBrowserLinkDirective
+	],
 	imports: [],
-	exports: [AccountTypeIconDirective,
-    InAppBrowserLinkDirective]
+	exports: [
+		InAppBrowserLinkDirective
+	]
 })
 export class DirectivesModule {}
