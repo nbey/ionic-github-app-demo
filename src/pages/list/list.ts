4d9759167aca046b4b8dba17316b9d520da7f326
Nafis Bey <nbey@jarv.us>
2018-05-10 23:32:30 -0400

    Add list with infinite scrolling
    - Load all GitHub users
    - Set red and blue font colors respectively for user and organizations

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { NavController, Events } from 'ionic-angular';
+import { AppStore } from '../../app/app.store';
+import {Store} from '@ngrx/store';
+import { LoadUsersAction } from '../../githubuser/githubuser.reducer';
+import { Subscription } from 'rxjs';
+import { SearchPage } from '../search/search';
 
 @Component({
     selector: 'page-list',
@@ -6,8 +12,45 @@ import { Component } from '@angular/core';
 })
 export class ListPage {
     items = <any>[];
+    private usersStateSubscription:Subscription;
 
-    constructor() {
+    constructor(
+        private store:Store<AppStore>,
+        public navCtrl: NavController,
+        public events: Events
+    ) {
 
+    }
+
+    ionViewDidEnter() {
+        this.usersStateSubscription = this.store
+            .select(state => state.githubUsersState.users)
+            .subscribe(users => {
+                this.items.push(...users);
+            });
+        if (!this.items.length) {
+            this.store.dispatch(new LoadUsersAction());
+        }
+    }
+
+    ionViewDidLeave() {
+        this.usersStateSubscription.unsubscribe();
+    }
+
+    doInfinite(infiniteScroll) {
+        const lastItem = this.items[this.items.length - 1];
+
+        this.store
+            .select(state => state.githubUsersState.loading)
+            .subscribe(isLoading => {
+                if (infiniteScroll && !isLoading) {
+                    infiniteScroll.complete();
+                }
+            });
+        this.store.dispatch(new LoadUsersAction(lastItem && lastItem.id));
+    }
+
+    itemSelected(user) {
+        this.events.publish('change-tab', user);
     }
 }
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 import { ListPage } from '../list/list';
 import { SearchPage } from '../search/search';
+import { NavParams, Events, NavController, Tabs} from 'ionic-angular';
 
 @Component({
     templateUrl: 'tabs.html'
 })
 export class TabsPage {
+    // reference component in page template
+    @ViewChild(Tabs) tabs: Tabs;
 
     tab1Root = ListPage;
     tab2Root = SearchPage;
 
-  constructor() {
+    constructor(
+        private navParams: NavParams,
+        private events: Events,
+        private navCtrl: NavController
+    ) {
+        events.subscribe('change-tab', (user) => {
+            this.searchParams.user = user;
+            this.tabs.select(1);
+        });
+        events.subscribe('reset-tab', () => {
+            this.searchParams.user = null;
+        });
+    }
 
-  }
 }
